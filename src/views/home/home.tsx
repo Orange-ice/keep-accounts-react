f16fb970ecf3130ada236a5117bac0f16b69b20c
Burt <2902978956@qq.com>
2022-03-05 18:29:34 +0800

    home 组件头部

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,6 +1,17 @@
+import React from 'react';
+import Icon from '../../components/icon';
+import styles from './home.module.scss';
+
 const Home = () => {
   return (
-    <div>主页</div>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.title}>
+          <Icon name="caidan" className={styles.menu}/>
+          <h3 className={styles.text}>豹富记账</h3>
+        </div>
+      </header>
+    </div>
   );
 };
 
