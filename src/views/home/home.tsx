be9a6c814864383601676907755195798838ae2c
Burt <2902978956@qq.com>
2022-03-18 21:22:41 +0800

    首页header部分静态样式

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Icon from '../../components/icon';
 import styles from './home.module.scss';
+import Icon from '../../components/icon';
 
 const Home = () => {
   return (
@@ -10,7 +10,30 @@ const Home = () => {
           <Icon name="caidan" className={styles.menu}/>
           <h3 className={styles.text}>豹富记账</h3>
         </div>
+
+        {/* tabs */}
+        <ul className={styles.tabs}>
+          <li className={styles.selected}>本月</li>
+          <li>上个月</li>
+          <li>今年</li>
+          <li>自定义起始时间</li>
+        </ul>
       </header>
+
+      <div className={styles.total}>
+        <div className={`${styles.details} ${styles.income}`}>
+          <span>收入</span>
+          <span>188</span>
+        </div>
+        <div className={`${styles.details} ${styles.expend}`}>
+          <span>支出</span>
+          <span>56</span>
+        </div>
+        <div className={`${styles.details}`}>
+          <span>净收入</span>
+          <span>132</span>
+        </div>
+      </div>
     </div>
   );
 };
